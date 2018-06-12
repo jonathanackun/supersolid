9f43130ca88f56b65279eb30004b2c11ee140849
Jonathan Ackun <jonathan.ackun@uk2group.com>
2018-06-12 17:16:38 +0100

    changes to app

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 var express = require('express');
 
+//var getScores = require('./ng/api/getScores.js');
+
+//var submitEntry = require('./ng/api/submitEntry');
+
+
 var app = express();
+
+var bodyParser = require('body-parser');
+
+app.use(bodyParser());
 
 app.use(express.static(__dirname));
 
-app.get('/', function(req, res) {
-	res.render('index.html');
-});
+app.use('/api', require('./api'));
+
+// app.get('/', function(req, res) {
+// 	res.render('index.html');
+// });
 
 var port = 3000;
 app.listen(port, function() {
